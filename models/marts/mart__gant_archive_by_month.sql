42d86419a700717ec50cbba6e53f632ce1270d15
Козуев Марсель Рахманалиевич <KozuevM@tsm.local>
2023-07-19 10:39:02 +0300

    return smr_ss and smr_sp

--- models/marts/mart__gant_archive_by_month.sql
+++ models/marts/mart__gant_archive_by_month.sql
@@ -9,7 +9,9 @@ WITH tmp as (
     SELECT
         code,
         start_year,
-        start_month, 
+        start_month,
+        smr_ss,
+        smr_sp, 
         "object",
         smr_type,
         "ZATRATY",
@@ -24,11 +26,14 @@ WITH tmp as (
         t1."code",
         t1."start_year",
         t1."start_month",
+        t1.smr_ss,
+        t1.smr_sp, 
         t1."object",
         t1.smr_type,
         t1."ZATRATY",
         t1."PRIBYL",
         t1."SMRFull"
+        
 
     FROM {{ ref('int__archive_by_month') }} t1
     
@@ -49,14 +54,16 @@ final as (
         g."Real",
         g."SNT_Knstr",
         g."SNT_KnstrE",
+        g."SNT_TypeKnstrE",
         g."SNT_Obj",
+        g."Num_Con",
 
         case 
             when g."SNT_Knstr" = 'Земляное полотно' then 'Земляное полотно'
             when g."SNT_Knstr" = 'Дорожная одежда' then 'Дорожная одежда'
             ELSE 'Прочие работы'
 
-        end as Конструктив
+        end as "Конструктив"
 
 
     FROM tmp t
