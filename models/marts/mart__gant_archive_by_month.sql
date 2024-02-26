e8136a095a1a9b797442704c4c5fb9dc312c0781
Egor Malyshev <egor.malyshev9817@gmail.com>
2024-02-26 20:05:37 +0300

    lint models

--- models/marts/mart__gant_archive_by_month.sql
+++ models/marts/mart__gant_archive_by_month.sql
@@ -1,17 +1,16 @@
-
 {{
     config(materialized='table')
 }}
 
 
-WITH tmp as (
+with tmp as (
 
-    SELECT
+    select
         code,
         start_year,
         start_month,
         smr_ss,
-        smr_sp, 
+        smr_sp,
         "object",
         smr_type,
         "ZATRATY",
@@ -19,49 +18,49 @@ WITH tmp as (
         "SMRFull",
         "UslGpd"
 
-    FROM {{ ref('int__gant_by_month') }}
+    from {{ ref('int__gant_by_month') }}
 
-        UNION ALL
+    union all
 
-    SELECT
+    select
         t1."code",
         t1."start_year",
         t1."start_month",
         t1.smr_ss,
-        t1.smr_sp, 
+        t1.smr_sp,
         t1."object",
         t1.smr_type,
         t1."ZATRATY",
         t1."PRIBYL",
         t1."SMRFull",
         "UslGpd"
-        
 
-    FROM {{ ref('int__archive_by_month') }} t1
-    
-    ),
+
+    from {{ ref('int__archive_by_month') }} as t1
+
+),
 
 
 
 final as (
-    SELECT t.*,
+    select
+        t.*,
 
-        CASE
-            WHEN length(CAST(start_month AS TEXT)) = 2 THEN CAST(start_month AS TEXT)
-            ELSE CONCAT('0', CAST(t.start_month AS TEXT))::text
-        END AS s_month,
+        case
+            when length(cast(start_month as text)) = 2 then cast(start_month as text)
+            else cast(concat('0', cast(t.start_month as text)) as text)
+        end as s_month,
 
         g."Name",
         g."Ispol",
         g."IspolUch",
-/*Временная мера для избавления от недопустимых значений поля Real*/
-        case g."Real" 
+        /*Временная мера для избавления от недопустимых значений поля Real*/
+        case g."Real"
             when null then null
             when 'Собственные силы' then 'Собственными силами'
             when 'Собственными силами' then 'Собственными силами'
             when 'Субподряд' then 'Силами субподрядных организаций'
             when 'Силами субподрядных организаций' then 'Силами субподрядных организаций'
-            else null
         end as "Силы реализации",
 
         {# g."Real", #}
@@ -77,24 +76,24 @@ final as (
             else ''
         end as "Силы реализации", #}
 
-        case 
+        case
             when g."SNT_Knstr" = 'Земляное полотно' then 'Земляное полотно'
             when g."SNT_Knstr" = 'Дорожная одежда' then 'Дорожная одежда'
-            ELSE 'Прочие работы'
+            else 'Прочие работы'
 
         end as "Конструктив",
 
-        vdc."Наименование работ и затрат",        
+        vdc."Наименование работ и затрат",
         vdc."Позиция из КВ",
         vdc."СМР П"
 
 
-    FROM tmp t
-        JOIN {{ source('spider', 'raw_spider__gandoper') }} g
-        ON t.code = g."Code" and t."object" = g."object" and g.project_type = 'проект'
+    from tmp as t
+    inner join {{ source('spider', 'raw_spider__gandoper') }} as g
+        on t.code = g."Code" and t."object" = g."object" and g.project_type = 'проект'
 
-        left join {{ ref('int__vdc_by_objects')}} vdc
+    left join {{ ref('int__vdc_by_objects') }} as vdc
         on g."Num_Con" = vdc."Шифр единичной расценки" and g."object" = vdc."object"
 )
 
-SELECT * FROM final
+select * from final
