99ee5ebc174ba53eeae4cd28e5b436016a47fd54
kozuevmarsel <koz1mars99@gmail.com>
2024-03-26 13:22:39 +0300

    fixed test

--- tests/test__duplication_of_oper.sql
+++ tests/test__duplication_of_oper.sql
@@ -6,11 +6,11 @@ with
         select
             pln."Code",  -- noqa
             mgabm.code,  -- noqa
-            coalesce("c_pln_SMRSsI", 0) as smr_ss_pln,
-            coalesce("c_pln_SMRSpI", 0) as smr_sp_pln,
+            {# coalesce("c_pln_SMRSsI", 0) as smr_ss_pln, #}
+            {# coalesce("c_pln_SMRSpI", 0) as smr_sp_pln, #}
             coalesce("c_pln_SMRSsI", 0) + coalesce("c_pln_SMRSpI", 0) as smr_full_pln,
-            sum(coalesce(mgabm.smr_ss, 0)),  -- noqa
-            sum(coalesce(mgabm.smr_sp, 0)),  -- noqa
+            {# sum(coalesce(mgabm.smr_ss, 0)),  -- noqa #}
+            {# sum(coalesce(mgabm.smr_sp, 0)),  -- noqa #}
             sum(coalesce(mgabm.smr_sp, 0))  -- noqa
             + sum(coalesce(mgabm.smr_ss, 0)) as smr_full_mart
         from {{ source("spider", "raw_spider__gandoper") }} as pln
@@ -27,7 +27,7 @@ with
         select *, smr_full_pln - smr_full_mart as delta
         from t1
         where
-            (smr_full_pln != 0 and smr_full_mart != 0)
+            (smr_full_pln != 0 or smr_full_mart != 0)
             and (smr_full_pln - smr_full_mart != 0)
     ),
 
@@ -37,44 +37,47 @@ with
 
     t1f as (
         select
-            "OperCode",
-            mgabm.code,
-            coalesce("c_aac_SMRSsI", 0) as smr_ss_fct,
-            coalesce("c_aac_SMRSpI", 0) as smr_sp_fct,
-            coalesce("c_aac_SMRSsI", 0) + coalesce("c_aac_SMRSpI", 0) as smr_full_fct,
-
-            sum(coalesce(mgabm.smr_ss, 0)),  -- noqa
-            sum(coalesce(mgabm.smr_sp, 0)),  -- noqa
-            sum(coalesce(mgabm.smr_sp, 0))  -- noqa
-            + sum(coalesce(mgabm.smr_ss, 0)) as smr_full_mart
+        "OperCode",
+        {# sum(coalesce("c_aac_SMRSsI", 0)) as smr_ss_fct, #}
+        {# sum(coalesce("c_aac_SMRSpI", 0)) as smr_sp_fct, #}
+        sum(coalesce("c_aac_SMRSsI", 0)) + sum(coalesce("c_aac_SMRSpI", 0)) as smr_full_fct
         from {{ source("spider", "raw_spider__archive") }} as fct
 
-        inner join
-            {{ ref("mart__gant_archive_by_month") }} as mgabm
-            on fct."OperCode" = mgabm.code
-
         where
-            fct.project_type = 'проект'
-            and mgabm.smr_type = 'факт'
-            and fct."ResCode" is not null
+    fct.project_type = 'проект'
+    and fct."ResCode" is null
 
-        group by mgabm.code, "OperCode", "c_aac_SMRSsI", "c_aac_SMRSpI"
-    ),
-
-    final_fct as (
-        select *, smr_full_fct - smr_full_mart as delta
-        from t1f
-        where
-            (smr_full_fct != 0 and smr_full_mart != 0)
-            and (smr_full_fct - smr_full_mart != 0)
+        group by "OperCode"
     ),
 
     fnlf as (
-        select *, abs(delta)  -- noqa
-        from final_fct
-        where abs(delta) > 1
-        order by delta, "OperCode"
-    )
+select 
+		t1f."OperCode",
+		mgabm.code,
+		{# t1f.smr_ss_fct, #}
+		{# t1f.smr_sp_fct, #}
+		t1f.smr_full_fct,
+        {# sum(coalesce(mgabm.smr_ss, 0)),  -- noqa #}
+        {# sum(coalesce(mgabm.smr_sp, 0)),  -- noqa #}
+        sum(coalesce(mgabm.smr_sp, 0))  -- noqa
+        + sum(coalesce(mgabm.smr_ss, 0)) as smr_full_mart,
+        t1f.smr_full_fct - (sum(coalesce(mgabm.smr_sp, 0)) 
+       						+ sum(coalesce(mgabm.smr_ss, 0))) 
+       						/* smr_full_mart */ as delta
+		
+		from t1f
+		inner join
+		"oup"."public"."mart__gant_archive_by_month" as mgabm
+        on t1f."OperCode" = mgabm.code 
+        where mgabm.smr_type = 'факт' 
+        group by 
+        t1f."OperCode",
+        mgabm.code,
+		{# t1f.smr_ss_fct, #}
+		{# t1f.smr_sp_fct, #}
+		t1f.smr_full_fct
+        having ((t1f.smr_full_fct - (sum(coalesce(mgabm.smr_sp, 0)) 
+       						+ sum(coalesce(mgabm.smr_ss, 0))))) not between -1 and 1)
 
 select count(*)
 from fnl
